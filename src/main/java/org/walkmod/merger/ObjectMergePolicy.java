3a8c539a01ba3f4fd5940ace8a3a477b6aec5cfa
rpau <rpau@ac.upc.edu>
2016-10-10 18:04:53 +0200

    bug on override policy

--- src/main/java/org/walkmod/merger/ObjectMergePolicy.java
+++ src/main/java/org/walkmod/merger/ObjectMergePolicy.java
@@ -18,25 +18,35 @@ package org.walkmod.merger;
 import java.util.Comparator;
 import java.util.List;
 
-public abstract class ObjectMergePolicy<T extends IdentificableNode>
-		implements
-			MergePolicy<T> {
+public abstract class ObjectMergePolicy<T extends IdentificableNode> implements MergePolicy<T> {
 
-	public void apply(T remoteObject, List<T> localList, List<T> resultList) {
-		Comparator<?> comparator = remoteObject.getIdentityComparator();
-		Object localObject = CollectionUtil.findObject(localList, remoteObject,
-				comparator);
-		this.apply((T) localObject, remoteObject, resultList);
-	}
+   public void apply(T remoteObject, List<T> localList, List<T> resultList) {
+      Comparator<?> comparator = remoteObject.getIdentityComparator();
+      Object localObject = CollectionUtil.findObject(localList, remoteObject, comparator);
+      this.apply((T) localObject, remoteObject, resultList);
+   }
 
-	public void apply(List<T> localList, List<T> remoteList,
-			@SuppressWarnings("rawtypes") List resultList){
-		if (remoteList != null) {
-			for (Object remoteObject : remoteList) {				
-				this.apply((T) remoteObject,localList, resultList);
-			}
-		}
-	}
-	
-	public abstract void apply(T localObject, T remoteObject, List<T> resultList);
+   public void apply(List<T> localList, List<T> remoteList, @SuppressWarnings("rawtypes") List resultList) {
+      if (remoteList != null) {
+         for (Object remoteObject : remoteList) {
+            this.apply((T) remoteObject, localList, resultList);
+         }
+      }
+      if (localList != null) {
+         for (T localObject : localList) {
+            Comparator<?> comparator = localObject.getIdentityComparator();
+            @SuppressWarnings("unchecked")
+            T remoteObject = (T) CollectionUtil.findObject(remoteList, localObject, comparator);
+            if (remoteObject == null) {
+               resultList.add(localObject);
+
+            }
+         }
+      } else {
+         resultList.addAll(localList);
+      }
+
+   }
+
+   public abstract void apply(T localObject, T remoteObject, List<T> resultList);
 }
